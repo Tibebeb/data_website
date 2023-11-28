f5c2005644f3ff55e1ace67663c6270f29f681e5
Tibebe <tibebesolomon633@gmail.com>
2023-11-28 17:15:18 +0300

    Hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
+import Hero from "./Component/Hero";
+
 
 function App() {
   return (
     <div>
       <Navbar/>
+      < Hero/>
     </div>
   );
 }
