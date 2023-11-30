c9885f7bf5353d2c65ccdcdbd89a30895e27fa8d
Tibebe <tibebesolomon633@gmail.com>
2023-11-30 10:55:22 +0300

    Newsletter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./Component/Navbar";
 import Hero from "./Component/Hero";
 import Analytics from "./Component/Analytics";
+import Newsletter from "./Component/Newsletter";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       < Hero/>
       <Analytics/>
+      <Newsletter/>
     </div>
   );
 }
