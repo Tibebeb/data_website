bcfe5a9496198996c1c328d863a1ff01a6ac9481
Tibebe <tibebesolomon633@gmail.com>
2023-12-01 10:45:30 +0300

    Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./Component/Hero";
 import Analytics from "./Component/Analytics";
 import Newsletter from "./Component/Newsletter";
 import Card from "./Component/Card";
+import Footer from "./Component/Footer";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Analytics/>
       <Newsletter/>
       <Card/>
+      <Footer/>
 
     </div>
   );
